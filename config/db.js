4528ba6c826eb653974b0753cc801cb13a725b59
VipraDThakkar <vipra.thakkar45@gmail.com>
2024-10-05 08:35:41 +0530

    commit 2

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://viprathakkar27:VIPRA1234@cluster0.xugbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    const constring = "XXXX";
+    const conn = await mongoose.connect(constring);
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );

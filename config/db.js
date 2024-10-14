7c59751480bf6440ab24b147e5aa99cc21fd7d44
VipraDThakkar <129350250+VipraDThakkar@users.noreply.github.com>
2024-10-14 15:07:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-<<<<<<< HEAD
     const constring = "xxxx";
-=======
-    const constring = "xxx";
->>>>>>> 19ff016d0f0b539808ffd0c18b6e4bab10f17943
     const conn = await mongoose.connect(constring);
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
